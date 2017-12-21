f33f530acd6ab09c2c5a2a7d7c560cc9a753aafb
DenisPashkov <pashkovdenis@gmail.com>
2017-12-21 15:40:00 +0200

    Add project files.

---
+++ MonteCarloLib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonteCarloLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
