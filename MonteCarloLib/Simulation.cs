57f9d9bbe9898a095ebd8838b070f07f8b5a515a
DenisPashkov <pashkovdenis@gmail.com>
2017-12-27 23:55:56 +0200

    stash

--- MonteCarloLib/Simulation.cs
+++ MonteCarloLib/Simulation.cs
@@ -9,50 +9,45 @@ namespace MonteCarloLib
 {
     public class Simulation
     {  
-        private Func<int, long, bool> _predicate;  
-        
+        private Func<int, long, bool> _predicate;   
         public Simulation(Func<int, long, bool> predicate)
         {
             _predicate = predicate; 
-        }       
-
-       /// <summary>
-       /// Simulate Here :  
-       /// </summary>
-       /// <param name="root"></param>
-       /// <param name="player"></param>
-       /// <param name="threads"></param>
-        public void Simulate(Node root, Player player, int threads=  4)
+        }   
+        public void Simulate(Node root, Player player )
         {
-            int number = 0;
+            int number = 0;  
             Stopwatch stop = Stopwatch.StartNew(); 
-            Queue<Node> quue = new Queue<Node>();   
-            //
+            Queue<Node> quue = new Queue<Node>();    
+
+            // _predicae :  
             while (_predicate(number,stop.ElapsedMilliseconds)) 
             {
-                quue.Enqueue(root);  
-                
-                while (quue.Count > 0)
-                { 
-                    Node current = quue.Dequeue() ;   
+                quue.Enqueue(root);
 
+                while (quue.Count > 0)
+                {   
+
+                    Node current = quue.Dequeue() ;  
+                    
                     // Selection  
                     if (current.ExpandedNodes.Count > 0)
                     {
                         current = current._ChildNodes.OrderByDescending(c => c.UCB1).FirstOrDefault();
                         current._ChildNodes.OrderByDescending(c => c.UCB1).ToList().ForEach(x => quue.Enqueue(x)); 
-                    }  
+                    }   
 
                     // Expansion 
                     if (current.Points > 0 && current._ChildNodes.Count > 0)
                     {
                         current.ExpandedNodes = current._ChildNodes;
                         current = current.ExpandedNodes.FirstOrDefault();
-                    }    
-                    // Rollout Phase  
+                    }      
+
+                    // Rollout    
                     while (!player.TakeAction(current, current.GetRandomAction()))
-                     player.AcceptLastMove();  
-                    
+                     player.AcceptLastMove();   
+                  
                     // back Propagation
                     while (current != null)
                     {
@@ -61,11 +56,14 @@ namespace MonteCarloLib
                             current.Points = 0;
                         current.Points += player.LastAction?._value ?? 0;
                         current = current.Parent;
-                    }    
+                    }  
+                    
                     number++; 
-                }
-                   
-            } 
+                } 
+            }  
+            
+
+
         }
 
 
