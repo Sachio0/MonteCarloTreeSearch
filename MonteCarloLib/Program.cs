4fc51aa0b31d708480be5ccbf929e17fd28f36d2
Den Pashkov <pashkovdenis@gmail.com>
2017-12-28 11:48:30 +0200

    Update Program.cs

--- MonteCarloLib/Program.cs
+++ MonteCarloLib/Program.cs
@@ -1,4 +1,4 @@
-﻿using MonteCarloLib.Models;
+using MonteCarloLib.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -15,15 +15,15 @@ namespace MonteCarloLib
             Console.WriteLine("Monte Carlo  Algorithm Tests");
 
             while (true)
-            { 
+            {  
 
-                //  Watch WriteLine:  
                 var watch = Stopwatch.StartNew(); 
-                 Console.WriteLine(); 
+                 Console.WriteLine();   
+
                 var s1 = new Node("nod1");
-                s1.AddAction(new NodeAction(10, true));  
+                s1.AddAction(new NodeAction(1, true)); 
                 var s2 = new Node("nod2");
-                s2.AddAction(new NodeAction(55, true));
+                s2.AddAction(new NodeAction(1, true));
                 s2.AddAction(new NodeAction(10, false)); 
                 var s3 = new Node("nod3");
                 s3.AddAction(new NodeAction(10, true));
@@ -36,13 +36,15 @@ namespace MonteCarloLib
                 s5.AddAction(new NodeAction(2, false)); 
                 var s6 = new Node("nod6");
                 s6.AddAction(new NodeAction(10, true)); 
+
                 var s7 = new Node("nod7");
-                s7.AddAction(new NodeAction(1000, true));   // Win 
+                s7.AddAction(new NodeAction(30, true));   // Win 
+
                 var s8 = new Node("Node8");
                 s8.AddAction(new NodeAction(1, true)); 
                 var s9 = new Node("Node9");
-                s9.AddAction(new NodeAction(1, true));  
-
+                s9.AddAction(new NodeAction(1, true));   
+                
                 s8.AddChild(s9);
                 s7.AddChild(s8);
                 s2.AddChild(s4);
@@ -51,18 +53,28 @@ namespace MonteCarloLib
                 s3.AddChild(s7);
                 s1.AddChild(s2);
                 s1.AddChild(s3);   
-
+                
                 var s10 = new Node("Node10");
                 s10.AddAction(new NodeAction(1, true));
-                s9.AddChild(s10); 
-                var simulation = new Simulation((int visit, long time) => visit < 100);
+                s9.AddChild(s10);  
+
+                var simulation = new Simulation((int visit, long time) => time < 2000);
                 simulation.Simulate(s1, new Player()); 
+
+
                 PrintTree(s1, "", false); 
                 Console.WriteLine($"Time : {watch.ElapsedMilliseconds}"); 
                 Console.ReadLine();
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tree"></param>
+        /// <param name="indent"></param>
+        /// <param name="last"></param>
+        /// <param name="weight"></param>
         static void PrintTree(Node tree, string indent, bool last, float weight = 0)
         {
 
