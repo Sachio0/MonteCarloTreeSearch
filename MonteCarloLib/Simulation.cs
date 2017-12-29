19d5e60e41377a908c5523caf6a6e205f0ae92cc
Den Pashkov <pashkovdenis@gmail.com>
2017-12-29 15:15:37 +0200

    Update Simulation.cs

--- MonteCarloLib/Simulation.cs
+++ MonteCarloLib/Simulation.cs
@@ -1,9 +1,8 @@
-﻿using MonteCarloLib.Models;
+using MonteCarloLib.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 
 namespace MonteCarloLib
 {
@@ -16,47 +15,53 @@ namespace MonteCarloLib
         }   
         public void Simulate(Node root, Player player )
         {
-            int number = 0;  
-            Stopwatch stop = Stopwatch.StartNew(); 
-            Queue<Node> quue = new Queue<Node>();    
+            int number = 0;
 
-            // _predicae :  
+            Stopwatch stop = Stopwatch.StartNew(); 
+            Queue<Node> quue = new Queue<Node>();
+              
             while (_predicate(number,stop.ElapsedMilliseconds)) 
             {
                 quue.Enqueue(root);
 
                 while (quue.Count > 0)
-                {   
+                {     
+                    Node current = quue.Dequeue();    
 
-                    Node current = quue.Dequeue() ;  
-                    
                     // Selection  
                     if (current.ExpandedNodes.Count > 0)
                     {
-                        current = current._ChildNodes.OrderByDescending(c => c.UCB1).FirstOrDefault();
-                        current._ChildNodes.OrderByDescending(c => c.UCB1).Skip(1).ToList().ForEach(x => quue.Enqueue(x));
-                    }   
-
+                        current = current._ChildNodes.OrderByDescending(c => c.UCB1).FirstOrDefault();                    
+                        current._ChildNodes.OrderByDescending(c => c.UCB1).ToList().ForEach(x => quue.Enqueue(x));
+                     
+                    }    
+                    
                     // Expansion 
                     if (current.Points > 0 && current._ChildNodes.Count > 0)
                     {
                         current.ExpandedNodes = current._ChildNodes;
                         current = current.ExpandedNodes.FirstOrDefault();
-                    }      
+                    }    
 
                     // Rollout    
                     while (!player.TakeAction(current, current.GetRandomAction()))
-                     player.AcceptLastMove();   
-                  
-                    // back Propagation
+                            player.AcceptLastMove();
+
+
+                    
+                    // Back Propogation
                     while (current != null)
-                    {
-                        current.NumberOfVisits++;
+                    { 
+                        
+                        current.NumberOfVisits++; 
                         if (current.Points == double.MaxValue)
-                            current.Points = 0;
+                            current.Points = 0;  
                         current.Points += player.LastAction?._value ?? 0;
+
                         current = current.Parent;
-                    }  
+                        
+                       
+                    }
                     
                     number++; 
                 } 
